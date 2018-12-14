0be720892271a21f298bef06a2971aa0b97ed7f5
HH小笼包 <1259747604@qq.com>
2018-12-14 23:12:15 +0800

    写了一个单纯打发时间的组件

--- blog/src/Pages/Home.vue
+++ blog/src/Pages/Home.vue
@@ -1,22 +1,47 @@
 <template>
     <div>
-        <h1>{{$store.state.session}}</h1>
-
+        <!--<h1>{{$store.state.session}}</h1>-->
+        <ul ref="list">
+            <li>
+                <v-njzy></v-njzy>
+            </li>
+            <li></li>
+            <li></li>
+            <li></li>
+        </ul>
     </div>
 </template>
 
 <script>
+    import njzy from '../components/interesting/njzy';
     export default {
         name: "",
+        components:{
+            vNjzy:njzy
+        },
+        created(){
+
+        },
         mounted(){
+            this.setHeight();
+        },
+        methods:{
+            setHeight(){
+                const len = this.$refs.list.children.length;
+                for(let i = 0; i < len; i++){
+                    this.$refs.list.children[i].style.height = `${window.innerHeight}px`;
+                }
+            }
         }
     }
 </script>
 
 <style scoped>
-    canvas{
-        display: block;
-        margin: 200px auto;
-        border: 1px solid #ddd;
+    ul > li{
+        width: 100%;
+    }
+    ul > li {
+        position: relative;
+        border-bottom:1px solid #e8eaec;
     }
 </style>
---
+++ blog/src/components/interesting/njzy.vue
@@ -0,0 +1,66 @@
+<template>
+    <div class="container">
+        <ul>
+            <li>
+                <span>致</span>
+            </li>
+            <li>
+                <div>
+                    <canvas class="one"></canvas>
+                </div>
+            </li>
+            <li>
+                <span>远</span>
+            </li>
+            <li style="border-bottom-right-radius: 42em 81em;">
+                <span>静</span>
+            </li>
+        </ul>
+    </div>
+</template>
+
+<script>
+    import one from '../../assets/one';
+    export default {
+        name: "",
+        mounted(){
+            this.one();
+        },
+        methods:{
+            one(){
+                one();
+            }
+        }
+    }
+</script>
+
+<style scoped>
+    ul{
+        position: absolute;
+        top: 50%;
+        left: 50%;
+        transform: translate3d(-50%,-50%,0);
+        display: flex;
+        width: 530px;
+        height: 430px;
+        flex-wrap: wrap;
+        justify-content: space-around;
+        align-content: space-around;
+        list-style: none;
+    }
+    ul >li {
+        position: relative;
+        width: 240px;
+        height: 200px;
+        /*background: #00a1d6;*/
+        border: 1px solid #c6d2c9;
+        border-radius: 15px;
+        line-height: 230px;
+        text-align: center;
+        overflow: hidden;
+    }
+    ul >li span{
+        font-family: 华文行楷;
+        font-size:17.5em;
+    }
+</style>
